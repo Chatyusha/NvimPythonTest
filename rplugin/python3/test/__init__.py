2a4ad737c0327d96249e733559943438893fe9c0
SecretCat <luckyhiroaki@icloud.com>
2021-01-01 11:25:48 +0000

    updat

--- rplugin/python3/test/__init__.py
+++ rplugin/python3/test/__init__.py
@@ -4,10 +4,13 @@ import pynvim
 class TestPlugin(object):
     def __init__(self, nvim):
         self.nvim = nvim
-
+    def echo(self, message):
+        self.nvim.command('echo \'' + message + '\'')
     @pynvim.command('TestCommand', nargs='*', range='')
     def testcommand(self, args, range):
-        self.nvim.current.line = ('Command with args: {}, range: {}'.format(args, range))
+       # self.nvim.current.line = ('Command with args: {}, range: {}'.format(args, range))
+       self.echo("hoge")
+
 
     @pynvim.function('TestFunction', sync=True)
     def testfunction(self, args):
